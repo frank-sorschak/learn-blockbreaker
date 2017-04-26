183760fee8df32900d5c099fbef3f1beaa3bac93
frank-sorschak <frank.sorschak@gmail.com>
2017-04-26 13:53:03 -0400

    Project Initial Setup
    Project Created and and Menus added

---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quit Game requested");
+        Application.Quit();
+    }
+
+
+}
---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+    // Use this for initialization
+
+    private void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+            Debug.Log("Duplicate Music Player Destroyed");
+        }
+        else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start() {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
